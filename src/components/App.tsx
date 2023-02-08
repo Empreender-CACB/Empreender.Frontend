e56fc7bb658db0ac62c2ad4e9528c4db711ba490
César Augusto <cesarisadog@gmail.com>
2023-02-08 20:20:12 -0300

    Update App.tsx

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,19 +4,22 @@ import Header from 'components/Header'
 import Banner from 'components/Banner'
 import Login from 'components/Login'
 import Main from 'pages/Main'
+import { AuthProvider } from 'context/AuthProvider'
 import { Routes, Route, BrowserRouter as Router } from 'react-router-dom'
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/" element={<Main />}>
-          <Route path="" element={<Banner />} />
-          <Route path="about" element={<Banner />} />
-        </Route>
-        <Route path="/settings" element={<Login />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Main />}>
+            <Route path="" element={<Banner />} />
+            <Route path="about" element={<Banner />} />
+          </Route>
+          <Route path="/settings" element={<Login />} />
+        </Routes>
+      </AuthProvider>
     </Router>
   )
 }
