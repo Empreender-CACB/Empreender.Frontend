c830a204bf61106c022eaeabd6bcbe61106a4040
César Augusto <cesarisadog@gmail.com>
2023-07-19 00:43:57 -0300

    build

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "path";
-import dynamicImport from 'vite-plugin-dynamic-import'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react({
-    babel: {
-      plugins: [
-        'babel-plugin-macros'
-      ]
-    }
-  }),
-  dynamicImport()],
-  assetsInclude: ['**/*.md'],
-  resolve: {
-    alias: {
-      '@': path.join(__dirname, 'src'),
-    },
-  },
-  build: {
-    outDir: 'build'
-  }
-});
+  plugins: [react(), tsconfigPaths()]
+})
