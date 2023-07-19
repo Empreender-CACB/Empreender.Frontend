c03948a0843c98262c38be66777d16680395b7e3
César Augusto <cesarisadog@gmail.com>
2023-07-19 16:20:32 -0300

    lol

--- dist/assets/_getPrototype-1c0d569b.js
+++
@@ -1,1 +0,0 @@
-import{a_ as r}from"./index-21b031a3.js";var t=r,e=t(Object.getPrototypeOf,Object),a=e;export{a as _};
--- dist/assets/index-f585d61a.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-21b031a3.js";const s=()=>e.jsx("div",{className:"flex flex-col gap-4 h-full",children:e.jsx("h1",{children:"INICIO"})}),o=s;export{o as default};
