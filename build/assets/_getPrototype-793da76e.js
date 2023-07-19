acfa89efb2f5959eb0f381964aac9e0e0fdfdfc9
EmanoelJohannes <emanoeljohannes@hotmail.com>
2023-07-19 00:18:16 -0300

    Build 2

---
+++ build/assets/_getPrototype-793da76e.js
@@ -0,0 +1,1 @@
+import{aZ as r}from"./index-75b8e25a.js";var t=r,e=t(Object.getPrototypeOf,Object),a=e;export{a as _};
--- build/assets/index-3724963c.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-43014adb.js";const s=()=>e.jsx("div",{className:"flex flex-col gap-4 h-full",children:e.jsx("h1",{children:"INICIO"})}),o=s;export{o as default};
