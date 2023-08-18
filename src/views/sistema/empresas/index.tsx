6afc20ef038c645adddb8c72b4f4a5eb2e18abd3
César Augusto <cesarisadog@gmail.com>
2023-08-18 02:27:15 -0300

    Update index.tsx

--- src/views/sistema/empresas/index.tsx
+++ src/views/sistema/empresas/index.tsx
@@ -4,6 +4,7 @@ import '@inovua/reactdatagrid-community/index.css'
 import { Link } from 'react-router-dom'
 import moment from 'moment'
 import DateFilter from '@inovua/reactdatagrid-community/DateFilter'
+import SelectFilter from '@inovua/reactdatagrid-community/SelectFilter'
 import '@inovua/reactdatagrid-community/theme/default-dark.css'
 import '@inovua/reactdatagrid-community/theme/green-light.css'
 import '@inovua/reactdatagrid-community/theme/blue-light.css'
@@ -22,9 +23,49 @@ import CustomReactDataGrid from '@/components/shared/CustomReactDataGrid'
 
 moment.locale('pt-br')
 
+const estadosBrasileiros=[
+    { sigla: 'AC', nome: 'ACRE' },
+    { sigla: 'AL', nome: 'ALAGOAS' },
+    { sigla: 'AP', nome: 'AMAPA' },
+    { sigla: 'AM', nome: 'AMAZONAS' },
+    { sigla: 'BA', nome: 'BAHIA' },
+    { sigla: 'CE', nome: 'CEARA' },
+    { sigla: 'DF', nome: 'DISTRITO FEDERAL' },
+    { sigla: 'ES', nome: 'ESPIRITO SANTO' },
+    { sigla: 'GO', nome: 'GOIAS' },
+    { sigla: 'MA', nome: 'MARANHAO' },
+    { sigla: 'MT', nome: 'MATO GROSSO' },
+    { sigla: 'MS', nome: 'MATO GROSSO DO SUL' },
+    { sigla: 'MG', nome: 'MINAS GERAIS' },
+    { sigla: 'PA', nome: 'PARA' },
+    { sigla: 'PB', nome: 'PARAIBA' },
+    { sigla: 'PR', nome: 'PARANA' },
+    { sigla: 'PE', nome: 'PERNAMBUCO' },
+    { sigla: 'PI', nome: 'PIAUI' },
+    { sigla: 'RJ', nome: 'RIO DE JANEIRO' },
+    { sigla: 'RN', nome: 'RIO GRANDE DO NORTE' },
+    { sigla: 'RS', nome: 'RIO GRANDE DO SUL' },
+    { sigla: 'RO', nome: 'RONDONIA' },
+    { sigla: 'RR', nome: 'RORAIMA' },
+    { sigla: 'SC', nome: 'SANTA CATARINA' },
+    { sigla: 'SP', nome: 'SAO PAULO' },
+    { sigla: 'SE', nome: 'SERGIPE' },
+    { sigla: 'TO', nome: 'TOCANTINS' }
+  ]
+  
+
+
 const columns = [
     { name: 'idempresa', header: 'ID', type: 'string' },
-    { name: 'nmuf', header: 'UF', type: 'string' },
+    { name: 'nmuf', header: 'UF', type: 'select',
+    filterEditor: SelectFilter,
+    filterEditorProps: {
+        multiple: true,
+        dataSource: estadosBrasileiros.map(state => {
+          return { id: state.nome, label: state.sigla}
+        }),
+      }
+    },
     { name: 'nmcidade', header: 'Cidade', type: 'string' },
     { name: 'restrita', header: 'Restrita', type: 'boolean' },
     {
@@ -72,6 +113,18 @@ const defaultFilterValue = [
         value: '',
     },
     {
+        name: 'nmuf',
+        operator:"inlist",
+        type: 'select',
+        value:''
+    },
+    {
+        name: 'nmcidade',
+        operator: 'contains',
+        type: 'string',
+        value: '',
+    },
+    {
         name: 'nmfantasia',
         operator: 'contains',
         type: 'string',
@@ -85,7 +138,6 @@ const defaultFilterValue = [
         type: 'string',
         value: '',
     },
-    { name: 'valor_antigo', operator: 'contains', type: 'string', value: '' },
 ]
 
 const Empresas = () => {
