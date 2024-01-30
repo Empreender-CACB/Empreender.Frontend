9ea6f52a335ada75bb25f996377e43e371af8737
EmanoelJohannes <emanoeljohannes@hotmail.com>
2024-01-30 13:33:46 -0300

    Adicionando outro estilo de navegação entre áreas

--- src/views/Inscricoes/esg/diagnostico/index.tsx
+++ src/views/Inscricoes/esg/diagnostico/index.tsx
@@ -203,62 +203,27 @@ const DiagnosticoEsg = () => {
                         <h1 className="text-2xl font-bold mb-4">Diagnóstico ESG</h1>
 
                         <nav aria-label="Progress">
-                            <ol role="list" className="border border-gray-300 rounded-md divide-y divide-gray-300 md:flex md:divide-y-0">
+                            <ol className="space-y-4 md:flex md:space-y-0 md:space-x-8">
                                 {diagnosticData.map((area, index) => (
-                                    <li key={area.id} className="relative md:flex-1 md:flex">
-                                        {currentArea === index ? (
-                                            // Área Atual
-                                            <a className="group flex items-center w-full">
-                                                <span className="px-6 py-4 flex items-center text-sm font-medium">
-                                                    <span className="flex-shrink-0 w-10 h-10 flex items-center justify-center bg-blue-600 rounded-full">
-                                                        <span className="text-white">{index + 1}</span>
-                                                    </span>
-                                                    <span className="ml-4 text-sm font-medium text-gray-900">{area.nome}</span>
-                                                </span>
-                                            </a>
-                                        ) : index < currentArea ? (
-                                            // Área Completada
-                                            <a className="group flex items-center w-full">
-                                                <span className="px-6 py-4 flex items-center text-sm font-medium">
-                                                    <span className="flex-shrink-0 w-10 h-10 flex items-center justify-center bg-indigo-600 rounded-full group-hover:bg-indigo-800">
-                                                        <CheckIcon className="w-6 h-6 text-white" aria-hidden="true" />
-                                                    </span>
-                                                    <span className="ml-4 text-sm font-medium text-gray-900">{area.nome}</span>
-                                                </span>
-                                            </a>
-                                        ) : (
-                                            // Área Futura
-                                            <a className="group flex items-center">
-                                                <span className="px-6 py-4 flex items-center text-sm font-medium">
-                                                    <span className="flex-shrink-0 w-10 h-10 flex items-center justify-center border-2 border-gray-300 rounded-full group-hover:border-gray-400">
-                                                        <span className="text-gray-500 group-hover:text-gray-900">{index + 1}</span>
-                                                    </span>
-                                                    <span className="ml-4 text-sm font-medium text-gray-500 group-hover:text-gray-900">{area.nome}</span>
-                                                </span>
-                                            </a>
-                                        )}
-
-                                        {index !== diagnosticData.length - 1 ? (
-                                            <div className="hidden md:block absolute top-0 right-0 h-full w-5" aria-hidden="true">
-                                                <svg
-                                                    className="h-full w-full text-gray-300"
-                                                    viewBox="0 0 22 80"
-                                                    fill="none"
-                                                    preserveAspectRatio="none"
-                                                >
-                                                    <path
-                                                        d="M0 -2L20 40L0 82"
-                                                        vectorEffect="non-scaling-stroke"
-                                                        stroke="currentcolor"
-                                                        strokeLinejoin="round"
-                                                    />
-                                                </svg>
-                                            </div>
-                                        ) : null}
+                                    <li key={area.id} className="md:flex-1">
+                                        <a
+                                            href="#"
+                                            className={`group pl-4 py-2 flex flex-col border-l-4 ${index === currentArea ? 'border-indigo-600' : 'border-gray-200'
+                                                } hover:border-indigo-800 md:pl-0 md:pt-4 md:pb-0 md:border-l-0 md:border-t-4`}
+                                        >
+                                            <span
+                                                className={`text-xs ${index === currentArea ? 'text-indigo-600' : 'text-gray-500'
+                                                    } font-semibold tracking-wide uppercase group-hover:text-indigo-800`}
+                                            >
+                                                {area.nome}
+                                            </span>
+                                        </a>
                                     </li>
                                 ))}
                             </ol>
                         </nav>
+
+                        
 
                         <div className="mt-8">
                             <div className='flex flex-col sm:flex-row justify-between items-center my-4'>
