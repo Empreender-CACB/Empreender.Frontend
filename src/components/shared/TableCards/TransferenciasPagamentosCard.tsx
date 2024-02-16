f376ebf543163eaf5673daecf7c08de66c85e37d
gabrieltomazz <gabrieltomazlima@hotmail.com>
2024-02-16 07:54:39 -0300

    feat: update card transf e pag

--- src/components/shared/TableCards/TransferenciasPagamentosCard.tsx
+++ src/components/shared/TableCards/TransferenciasPagamentosCard.tsx
@@ -1,4 +1,4 @@
-import TagActiveInative from '@/components/ui/Tag/TagActiveInative'
+import { maskMoney } from '@/utils/MaskMoney'
 import { Link } from 'react-router-dom'
 
 export const TransferenciasPagamentosCard = ({ data }) => {
@@ -10,13 +10,12 @@ export const TransferenciasPagamentosCard = ({ data }) => {
             offset={-80}
         >
             <div className="w-full flex justify-between">
-                <h5>{data.idlanc} </h5>
-                <TagActiveInative value={data["lancamento.flativo"]} activeText="S" />
+                <h5>{maskMoney(data.vllanc)} Lanc: {data.idlanc} </h5>
             </div>
 
-            <span>{data.idprojeto+' - '+data.idlanc  || '-'}</span>
+            <span>Projeto: {data.idprojeto+' - Origem: '+data.tpfonterec  || '-'}</span>
             <p className="mt-2">
-                {data.nmcidade} - {data.iduf}
+                {data.nmcredor}
             </p>
         </Link>
     )
