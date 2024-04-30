cbab7bbcf4588a158756006b7fff402b07b4fe53
EmanoelJohannes <emanoeljohannes@hotmail.com>
2024-04-30 08:40:37 -0300

    Update diagnosticos.tsx

--- src/views/ranking/diagnosticos.tsx
+++ src/views/ranking/diagnosticos.tsx
@@ -1,4 +1,4 @@
-import { Table } from '@/components/ui';
+import { Table, Tooltip } from '@/components/ui';
 import TBody from '@/components/ui/Table/TBody';
 import THead from '@/components/ui/Table/THead';
 import Td from '@/components/ui/Table/Td';
@@ -6,6 +6,14 @@ import Th from '@/components/ui/Table/Th';
 import Tr from '@/components/ui/Table/Tr';
 import ApiService from '@/services/ApiService';
 import React, { useEffect, useState } from 'react';
+
+import { RiTeamLine } from "react-icons/ri";
+import { BsFillHouseGearFill } from "react-icons/bs";
+import { TbPigMoney } from "react-icons/tb";
+import { FaUsers } from "react-icons/fa";
+import { FaHandshakeSimple } from "react-icons/fa6";
+import { GiProgression } from "react-icons/gi";
+import { FaHandHoldingUsd } from "react-icons/fa";
 
 interface NotaPorArea {
   [key: string]: string;
@@ -19,6 +27,7 @@ interface Diagnostico {
   id: number;
   entidade: string;
   cidade: string;
+  uf: string;
   notasPorArea: NotaPorArea;
   somaTotalNotas: number;
   rankingGeral: string;
@@ -58,7 +67,6 @@ function RankingDiagnostico() {
           throw new Error('Falha ao buscar dados');
         }
       } catch (error: any) {
-        console.error(error);
         setState({
           diagnosticos: [],
           isLoading: false,
@@ -69,6 +77,56 @@ function RankingDiagnostico() {
 
     getDados();
   }, []);
+
+  function renderPodium(diagnostico: any) {
+    let heightClass = '';
+    switch (diagnostico.rankingGeral) {
+      case "1º":
+        heightClass = 'h-52';
+        break;
+      case "2º":
+        heightClass = 'h-40';
+        break;
+      case "3º":
+        heightClass = 'h-32';
+        break;
+      default:
+        heightClass = 'h-32';
+    }
+
+    return (
+      <div key={diagnostico.id} className="text-center">
+        <p className="mb-2 font-bold">{diagnostico.cidade}</p>
+        <div className={`bg-green-800 w-36 ${heightClass} flex justify-center items-end pb-2 rounded`}>
+          <span className="text-white text-6xl font-semibold pb-8">{diagnostico.rankingGeral}</span>
+        </div>
+        <p className="mt-2">{parseFloat(diagnostico.notaGeral).toFixed(2)}</p>
+      </div>
+    );
+  }
+
+
+  const iconStyles = "text-white text-2xl";
+
+  const iconMapping: any = {
+    "Pessoas": <RiTeamLine className={iconStyles} />,
+    "Processos internos": <BsFillHouseGearFill className={iconStyles} />,
+    "Finanças": <TbPigMoney className={iconStyles} />,
+    "Clientes": <FaUsers className={iconStyles} />,
+    "Cultura associativista": <FaHandshakeSimple className={iconStyles} />,
+    "Desenvolvimento local": <GiProgression className={iconStyles} />,
+    "Benefícios": <FaHandHoldingUsd className={iconStyles} />
+  };
+
+  const colorMapping: any = {
+    "Pessoas": "bg-red-500",
+    "Processos internos": "bg-blue-500",
+    "Finanças": "bg-green-500",
+    "Clientes": "bg-yellow-500",
+    "Cultura associativista": "bg-pink-500",
+    "Desenvolvimento local": "bg-purple-500",
+    "Benefícios": "bg-indigo-500"
+  };
 
   if (state.isLoading) return <p>Carregando...</p>;
   if (state.error) return <p>Erro: {state.error}</p>;
@@ -81,7 +139,7 @@ function RankingDiagnostico() {
     <div className="flex flex-col items-center">
 
       <div className="w-full py-16 flex items-center" style={{ background: '#76A030' }}>
-        <div className="max-w-6xl mx-auto w-full flex justify-between items-center px-4 sm:px-6 lg:px-8">
+        <div className="max-w-screen-xl mx-auto w-full flex justify-between items-center">
           <div>
             <h1 className="text-white text-5xl font-bold tracking-tight">Ranking de Entidades 2024</h1>
             <div className="w-64 h-1 bg-white mt-8"></div>
@@ -95,7 +153,7 @@ function RankingDiagnostico() {
       </div>
 
       {/* Podium Component */}
-      <div className='max-w-screen-lg	'>
+      <div className='max-w-screen-xl'>
         <h2 className="mt-10 text-3xl mb-4 font-semibold">Top 3 Entidades</h2>
         <p>O ranking geral do Índice de Cidades Empreendedoras (ICE) 2023 leva em consideração cada um dos sete determinantes apresentados no
           relatório e o seu resultado é um instrumento de avaliação voltado para gestores públicos e organizações de apoio interessadas em
@@ -104,32 +162,17 @@ function RankingDiagnostico() {
 
         <div className="flex justify-center w-full">
           <div className="flex justify-center items-end mt-5 gap-8">
-            {topTresDiagnosticos.map((diagnostico, index) => {
-              let heightClass = '';
-              switch (diagnostico.rankingGeral) {
-                case "1º":
-                  heightClass = 'h-52'; 
-                  break;
-                case "2º":
-                  heightClass = 'h-40';
-                  break;
-                case "3º":
-                  heightClass = 'h-32'; 
-                  break;
-                default:
-                  heightClass = 'h-32';
-              }
+            {topTresDiagnosticos
+              .filter((diagnostico) => diagnostico.rankingGeral === "2º")
+              .map((diagnostico) => renderPodium(diagnostico))}
 
-              return (
-                <div key={index} className="text-center">
-                  <p className="mb-2 font-bold">{diagnostico.cidade}</p>
-                  <div className={`bg-green-800 w-36 ${heightClass} flex justify-center items-end pb-2 rounded`}>
-                    <span className="text-white text-6xl font-semibold pb-8">{diagnostico.rankingGeral}</span>
-                  </div>
-                  <p className="mt-2">{parseFloat(diagnostico.notaGeral).toFixed(2)}</p>
-                </div>
-              );
-            })}
+            {topTresDiagnosticos
+              .filter((diagnostico) => diagnostico.rankingGeral === "1º")
+              .map((diagnostico) => renderPodium(diagnostico))}
+
+            {topTresDiagnosticos
+              .filter((diagnostico) => diagnostico.rankingGeral === "3º")
+              .map((diagnostico) => renderPodium(diagnostico))}
           </div>
         </div>
 
@@ -139,27 +182,32 @@ function RankingDiagnostico() {
             <Table>
               <THead>
                 <Tr>
-                  {/* Inserir cabeçalhos com ícones, você precisará adicionar ícones relevantes */}
                   <Th>Entidade</Th>
-                  <Th>Cidade</Th>
+                  <Th>Cidade/UF</Th>
                   <Th>Total</Th>
-                  {/* Cabeçalhos dinâmicos baseados nas áreas */}
-                  {state.diagnosticos[0] && Object.entries(state.diagnosticos[0].areas).map(([key, name], index) => (
-                    <Th key={key}>
-                      <span className={`icon-${name.toLowerCase().replace(/\s/g, '-')}`}>{name}</span>
+                  {state.diagnosticos[0] && Object.entries(state.diagnosticos[0].areas).map(([key, name]) => (
+                    <Th key={key} >
+                      <div className='flex flex-col items-center justify-center py-1'>
+                        <div className={`rounded-full p-2 ${colorMapping[name]} flex justify-center items-center`}>
+                          {iconMapping[name]}
+                        </div>
+                        <span className="mt-2" style={{fontSize: '10px', textAlign: 'center'}}>{name}</span>
+                      </div>
                     </Th>
                   ))}
+
+
+
                 </Tr>
               </THead>
               <TBody>
                 {state.diagnosticos.map((diagnostico, index) => (
                   <Tr key={index}>
                     <Td>{diagnostico.entidade}</Td>
-                    <Td>{diagnostico.cidade}</Td>
-                    <Td>{diagnostico.rankingGeral} - {diagnostico.notaGeral}</Td>
-
+                    <Td>{diagnostico.cidade} - {diagnostico.uf}</Td>
+                    <Td className="whitespace-nowrap"><strong>{diagnostico.rankingGeral} - {diagnostico.notaGeral}</strong></Td>
                     {Object.keys(diagnostico.areas).map(areaId => (
-                      <Td key={areaId}>{diagnostico.notasPorArea[areaId]}</Td>
+                      <Td className="whitespace-nowrap" key={areaId}>{diagnostico.notasPorArea[areaId]}</Td>
                     ))}
                   </Tr>
                 ))}
