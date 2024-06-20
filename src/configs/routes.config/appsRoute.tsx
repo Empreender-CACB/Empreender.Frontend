ad645c467ae3d1fc371d0c588c5e32afd3dd7ae6
EmanoelJohannes <emanoeljohannes@hotmail.com>
2024-06-20 13:24:19 -0300

    Ajustes

--- src/configs/routes.config/appsRoute.tsx
+++ src/configs/routes.config/appsRoute.tsx
@@ -136,6 +136,9 @@ const appsRoute: Routes = [
         key: 'sistemaPrestcontas.listaGeralLancamentos',
         path: `${APP_PREFIX_PATH}/prestcontas/lista-geral-lancamentos`,
         component: lazy(() => import('@/views/sistema/prestcontas/acompanhamento-financeiro/lista-geral-lancamentos')),
+        meta: {
+            pageContainerType: 'gutterless',
+        },
     },
 ]
 
