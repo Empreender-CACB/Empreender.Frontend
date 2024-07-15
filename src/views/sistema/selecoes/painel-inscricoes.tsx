1d473c86f322a5dde37f8d9640713a87d1ea21ca
EmanoelJohannes <emanoeljohannes@hotmail.com>
2024-07-15 12:06:15 -0300

    Botão de voltar

--- src/views/sistema/selecoes/painel-inscricoes.tsx
+++ src/views/sistema/selecoes/painel-inscricoes.tsx
@@ -1,11 +1,25 @@
 import ZohoFrame from '@/components/shared/ZohoFrame'
+import { Button } from '@/components/ui'
+import { HiOutlineReply } from 'react-icons/hi'
+import { Link } from 'react-router-dom'
 
 const PainelInscrições = () => {
     const zohoURL = `https://analytics.zoho.com/open-view/1704802000045008185/862aff329a8285119d52916659eacc87`
 
     return (
         <div>
-            <h2 className="mb-4">Painel de inscrições</h2>
+            <div className="lg:flex items-center justify-between mb-4">
+                <h2 className="mb-4">Painel de inscrições</h2>
+                <Button size="sm" icon={<HiOutlineReply />}>
+                    <Link
+                        className="menu-item-link"
+                        to={`/sistema/selecoes/e2022-consultores`}
+                    >
+                        Voltar
+                    </Link>
+                </Button>
+            </div>
+            
             <ZohoFrame url={zohoURL} />
         </div>
     )
