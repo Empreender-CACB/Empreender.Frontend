29ea21647967fee1f0227dd3b45fa7350b0c3212
EmanoelJohannes <emanoeljohannes@hotmail.com>
2024-08-31 09:36:47 -0300

    Mais ajustes

--- src/views/sistema/usuarios/index.tsx
+++ src/views/sistema/usuarios/index.tsx
@@ -1,24 +1,18 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import '@inovua/reactdatagrid-community/index.css'
-
 import { GoSignIn } from 'react-icons/go'
-
 import { Link } from 'react-router-dom'
-import moment from 'moment'
-import { Button } from '@/components/ui'
+import { Button, Tooltip } from '@/components/ui'
 import { AdaptableCard } from '@/components/shared'
-
-import 'moment/locale/pt-br'
 import CustomReactDataGrid from '@/components/shared/CustomReactDataGrid'
-
-import { HiOutlineReply, HiPlusCircle } from 'react-icons/hi'
+import { HiOutlineEye, HiOutlineReply, HiPlusCircle } from 'react-icons/hi'
 import { setUser, signInSuccess, useAppDispatch } from '@/store'
 import { UsuariosCard } from '@/components/shared/TableCards/UsuariosCard'
 import TagActiveInative from '@/components/ui/Tag/TagActiveInative'
 import { apiEntrarComo } from '@/services/MenuService'
-
-moment.locale('pt-br')
-
+// import useThemeClass from '@/utils/hooks/useThemeClass'
+import SelectFilter from '@inovua/reactdatagrid-community/SelectFilter'
+// import TagTrueFalse from '@/components/ui/Tag/TagTrueFalse'
 const defaultFilterValue = [
     { name: 'iduf', operator: 'contains', type: 'string', value: '' },
     { name: 'nmcidade', operator: 'contains', type: 'string', value: '' },
@@ -29,8 +23,14 @@ const defaultFilterValue = [
     { name: 'flativo', operator: 'contains', type: 'string', value: '' },
 ]
 
+const activeValue = [
+    { name: 'Ativo', value: 'S' },
+    { name: 'Inativo', value: 'N' },
+]
+
 const Usuarios = () => {
     const dispatch = useAppDispatch()
+    // const { textTheme } = useThemeClass()
 
     const handleEntrarComo = async (userData: any) => {
         const cpf = userData.nucpf.replace(/[.-]/g, '')
@@ -60,54 +60,94 @@ const Usuarios = () => {
             const encodedCredentials = btoa(
                 `${adaptedUser.nucpf}:${adaptedUser.nmusuario}`
             )
-            window.location.href = `${
-                import.meta.env.VITE_PHP_URL
-            }/sistema/adminutils/trocar-usuario?credentials=${encodedCredentials}`
+            window.location.href = `${import.meta.env.VITE_PHP_URL
+                }/sistema/adminutils/trocar-usuario?credentials=${encodedCredentials}`
         } catch (error) {
             console.error('Erro ao tentar entrar como outro usuário', error)
         }
     }
 
     const columns = [
-        { name: 'iduf', header: 'UF', type: 'string', flex: 0.4 },
+        { name: 'iduf', header: 'UF', type: 'string', flex: 0.4, minWidth: 80 },
         { name: 'nmcidade', header: 'Cidade', defaultFlex: 1 },
         { name: 'nucpf', header: 'CPF', defaultFlex: 1 },
         {
             name: 'nmusuario',
-            header: 'Nome Usuário',
+            header: 'Nome do usuário',
             defaultFlex: 1.5,
-            render: ({ data }: any) => (
-                <div
-                    style={{
-                        display: 'flex',
-                        justifyContent: 'space-between',
-                        alignItems: 'center',
-                    }}
+            render: ({ data, value }: any) => (
+                <Link
+                    className="menu-item-link max-w-md text-blue-500"
+                    to={`${import.meta.env.VITE_PHP_URL
+                        }/sistema/usuario/detalhe/uid/${btoa(data.nucpf)}`}
                 >
-                    {data.nmusuario}
-                    <button
-                        style={{
-                            background: 'none',
-                            border: 'none',
-                            cursor: 'pointer',
-                        }}
-                        onClick={() => handleEntrarComo(data)}
-                    >
-                        <GoSignIn size={20} title="Entrar como" />
-                    </button>
-                </div>
+                    {value}
+                </Link>
             ),
         },
-        { name: 'nmlogin', header: 'Login', defaultFlex: 1 },
-        { name: 'nome', header: 'Perfil', defaultFlex: 1 },
+        {
+            name: 'nmlogin', header: 'Login', defaultFlex: 1,
+            render: ({ data, value }: any) => (
+                <Link
+                    className="menu-item-link max-w-md text-blue-500"
+                    to={`${import.meta.env.VITE_PHP_URL
+                        }/sistema/usuario/detalhe/uid/${btoa(data.nucpf)}`}
+                >
+                    {value}
+                </Link>
+            ),
+        },
+        {
+            name: 'nome', header: 'Perfil', defaultFlex: 1,
+            render: ({ data, value }: any) => (
+                <Link
+                    className="menu-item-link max-w-md text-blue-500"
+                    to={`${import.meta.env.VITE_PHP_URL
+                        }/sistema/usuario/detalhe/uid/${btoa(data.nucpf)}`}
+                >
+                    {value}
+                </Link>
+            ),
+        },
         {
             name: 'flativo',
             header: 'Ativo',
-            defaultFlex: 0.6,
+            type: 'select',
+            operator: 'equals',
+            value: '',
+            filterEditor: SelectFilter,
+            filterEditorProps: {
+                dataSource: activeValue.map((option) => {
+                    return { id: option.value, label: option.name }
+                }),
+            },
             render: ({ value }: any) => (
                 <div className="flex items-center justify-center">
                     <TagActiveInative value={value} activeText="S" />
                 </div>
+            ),
+        },
+        {
+            name: 'id',
+            header: 'Ações',
+            defaultFlex: 0.6,
+            render: ({ data }: any) => (
+                <>
+
+                    <div className="flex justify-center text-lg">
+                        <Tooltip title="Entrar como">
+                            <span
+                                // className={`cursor-pointer p-2 hover:${textTheme}`}
+                                className="cursor-pointer p-2"
+                                onClick={() => handleEntrarComo(data)}
+                            >
+                                <GoSignIn />
+                            </span>
+                        </Tooltip>
+                    </div>
+
+                </>
+
             ),
         },
     ]
@@ -117,7 +157,7 @@ const Usuarios = () => {
             <div className="lg:flex items-center justify-between mb-4">
                 <h3 className="mb-4 lg:mb-0">Usuários</h3>
                 <div className="flex flex-col lg:flex-row lg:items-center">
-                <Button size="sm" icon={<HiOutlineReply />}>
+                    <Button size="sm" icon={<HiOutlineReply />}>
                         <Link
                             className="menu-item-link"
                             to={`${import.meta.env.VITE_PHP_URL}/sistema/usuario`}
