173901fef499e9706438b1e8cec186515eec98c3
César Augusto <cesarisadog@gmail.com>
2025-01-29 09:03:08 -0300

    Update index.tsx

--- src/views/sistema/cogecom/detalhes/index.tsx
+++ src/views/sistema/cogecom/detalhes/index.tsx
@@ -7,9 +7,15 @@ import CustomerProfile from './components/CustomerProfile'
 import PaymentHistory from './components/PaymentHistory'
 import CurrentSubscription from './components/CurrentSubscription'
 import PaymentMethods from './components/PaymentMethods'
-import isEmpty from 'lodash/isEmpty'
-import useQuery from '@/utils/hooks/useQuery'
 
+import { Button } from '@/components/ui';
+import {
+
+  AiOutlineCheck,
+  AiOutlineClose,
+  AiOutlineExclamationCircle,
+
+} from 'react-icons/ai';
 
 const CustomerDetail = () => {
 
@@ -17,6 +23,23 @@ const CustomerDetail = () => {
     return (
         <Container className="h-full">
 
+<div className="flex justify-between items-center mb-8">
+          <h1 className="text-3xl font-bold text-gray-900">Detalhes da inscrição - #0077b5</h1>
+          <div className="flex space-x-2">
+          <Button className="mb-2 flex items-center" variant="solid" color="green-600">
+              <AiOutlineCheck className="mr-2" /> Aprovar Candidatura
+            </Button>
+
+            <Button className="mb-2 flex items-center" variant="solid" color="yellow-600">
+              <AiOutlineExclamationCircle className="mr-2" /> Informar Pendência
+            </Button>
+            <Button className="mb-2 flex items-center" variant="solid" color="red-600">
+              <AiOutlineClose className="mr-2" /> Recusar Candidatura
+            </Button>
+
+
+          </div>
+        </div>
                     <div className="flex flex-col xl:flex-row gap-4">
                         <div>
                             <CustomerProfile/>
