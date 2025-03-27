3ab0119c068ae53f4b3000abfbafe688d576d38f
EmanoelJohannes <emanoeljohannes@hotmail.com>
2025-03-27 15:42:35 -0300

    Update SignIn.tsx

--- src/views/auth/SignIn/SignIn.tsx
+++ src/views/auth/SignIn/SignIn.tsx
@@ -1,32 +1,6 @@
 import SignInForm from './SignInForm';
 
 const SignIn = () => {
-    // const [isMobile, setIsMobile] = useState(false);
-
-    // useEffect(() => {
-    //     const handleResize = () => {
-    //         setIsMobile(window.innerWidth <= 768);
-    //     };
-    //     handleResize();
-    //     window.addEventListener('resize', handleResize);
-
-    //     return () => window.removeEventListener('resize', handleResize);
-    // }, []);
-
-    // if (isMobile) {
-    //     return (
-    //         <div className="mobile-message text-center p-4">
-    //             <div className="logo mb-8 flex justify-center">
-    //                 <img className="h-11" src="/img/logo/EMPREENDER+CACB.png" alt="Empreender e CACB" /> 
-    //             </div>
-    //             <p><strong>Estamos em obras!</strong></p>
-    //             <p>Vamos trocar de pneu. Andando.</p>
-    //             <p>A viagem levará algum tempo, precisamos ir devagar, mas confiamos que chegaremos bem. Aos poucos teremos um novo portal.</p>
-    //             <p className="mt-4"><strong>O portal por enquanto estará disponível apenas na versão desktop.</strong></p>
-    //         </div>
-    //     );
-    // }
-
     return (
         <>
             <div className="mb-8 flex center justify-center">
